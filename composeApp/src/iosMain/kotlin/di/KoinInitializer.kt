a459a3ef41c186db1c390a64d892ef1a616d8ead
victorlpgazolli <victorlpgazolli@gmail.com>
2024-06-04 23:36:20 -0300

    ref: init home view module from koin android and ios

--- composeApp/src/iosMain/kotlin/di/KoinInitializer.kt
+++ composeApp/src/iosMain/kotlin/di/KoinInitializer.kt
@@ -1,11 +1,13 @@
 package di
 
+import com.example.home.di.dataModule
+import com.example.home.di.homeModelModule
 import org.koin.core.context.startKoin
 
 actual class KoinInitializer {
     actual fun init() {
         startKoin{
-            modules(dataModule, viewModelModule)
+            modules(dataModule, viewModelModule, homeModelModule)
         }
     }
 }
