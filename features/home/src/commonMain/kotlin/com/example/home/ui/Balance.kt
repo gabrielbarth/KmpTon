a7fa6eba57580ac6189d76c6d04b94bb0a332783
victorlpgazolli <victorlpgazolli@gmail.com>
2024-06-15 18:06:39 -0300

    ref: add style for balance

--- features/home/src/commonMain/kotlin/com/example/home/ui/Balance.kt
+++ features/home/src/commonMain/kotlin/com/example/home/ui/Balance.kt
@@ -1,33 +1,138 @@
 package com.example.home.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.theme.AppTheme
+import com.example.theme.resources.Res
+import com.example.theme.resources.arrow_right
+import com.example.theme.resources.menu_hamburger
+import com.example.theme.resources.toggle_visibility
+import com.example.theme.resources.wait_clock
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
-fun Balance(modifier: Modifier = Modifier) {
+fun Balance(
+    futurePayable: Double = 103.20,
+    currentBalance: Double = 1320.20,
+    modifier: Modifier = Modifier
+) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(
-            modifier = Modifier.padding(top = 12.dp, bottom = 10.dp),
-            text = "Future Balance"
-        )
+        Row(
+            modifier = modifier
+                .height(40.dp)
+                .fillMaxWidth().padding(AppTheme.space.large).clickable {
+
+                },
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(Res.drawable.wait_clock),
+                contentDescription = "Icone relógio",
+                modifier = modifier.padding(end = AppTheme.space.medium)
+            )
+            Text(
+                modifier = modifier,
+                fontSize = AppTheme.textSize.medium,
+                text = "R\$ $futurePayable",
+                fontWeight = FontWeight(700),
+            )
+            Text(
+                modifier = modifier,
+                fontSize = AppTheme.textSize.medium,
+                text = " em vendas a receber"
+            )
+            Image(
+                painter = painterResource(Res.drawable.arrow_right),
+                contentDescription = "Icone relógio",
+                modifier = modifier.padding(start = AppTheme.space.medium)
+            )
+        }
         Divider(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxWidth()
         )
-        Text(
-            modifier = Modifier.padding(12.dp),
-            text = "Current Balance"
-        )
+        Row(
+            modifier = modifier
+                .fillMaxWidth().padding(
+                    top = 20.dp,
+                    start = 20.dp,
+                    end = 20.dp,
+                    bottom = 0.dp
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth(0.5f),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    modifier = modifier.padding(bottom = AppTheme.space.small),
+                    text = "Dinheiro na carteira",
+                    color = Color.Gray,
+                    fontWeight = FontWeight(700),
+                )
+                Row(
+                    modifier = modifier,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        modifier = modifier,
+                        text = "R\$ $currentBalance",
+                        fontWeight = FontWeight(700),
+                        fontSize = 28.sp
+                    )
+                    Image(
+                        painter = painterResource(Res.drawable.toggle_visibility),
+                        contentDescription = "Icone esconder saldo",
+                        modifier = modifier.padding(start = AppTheme.space.medium).clickable {
+
+                        },
+                    )
+                }
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = modifier.padding(AppTheme.space.medium),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.LightGray
+                )
+            ) {
+                Text(
+                    text = "Ver extrato",
+                    color = Color.Black,
+                    fontSize = AppTheme.textSize.medium,
+                )
+            }
+        }
+
     }
 }
